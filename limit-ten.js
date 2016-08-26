600c7b17a741ca2384018da709a615d29b53f624
John Hamm <johndhamm1@gmail.com>
2016-08-26 11:34:02 -0500

    limits to 10 results, requirements met

--- limit-ten.js
+++ limit-ten.js
@@ -1,10 +1,12 @@
 // You will also create your own Transform stream as a module called limit-ten.js. This Transform should be a limiter before finally pipeing the results to stdout.
 
 const { Transform } = require('stream');
+let counter = 0;
 
 module.exports = Transform({
 	transform (buff, enc, cb) { //enhanced object literal
-		// cb(null, buff.toString().toUpperCase())
-		// cb(null, buff?????)
+		counter ++;
+		// console.log("counter: ", counter);
+		counter < 11 ? cb(null, buff) : cb(null, null); // Limit the results to only ten words
 	}
 });
